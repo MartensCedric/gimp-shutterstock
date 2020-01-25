7969fad93c25cdc7390f31c327a16deacbe4b983
MartensCedric <cedricmartens98@gmail.com>
2020-01-25 14:37:16 -0500

    Fix script

--- gui/update_gimp.sh
+++ gui/update_gimp.sh
@@ -1,2 +1,3 @@
+mkdir -p ~/.gimp-2.8/plug-ins/gui/
 cp shutterstock_gui.py ~/.gimp-2.8/plug-ins/gui/shutterstock_gui.py
-cp shutterstock.py ~/.gimp-2.8/plug-ins/shutterstock.py
+cp ../shutterstock.py ~/.gimp-2.8/plug-ins/shutterstock.py
