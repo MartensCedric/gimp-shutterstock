03a9147e67df9f7a414c114daadcbed3adce459c
ninjaweeaboo <williamwells2013@hotmail.com>
2020-01-25 11:29:40 -0500

    Add basic gui

---
+++ shutterstock.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from gimpfu import *
+import os
+
+
+def start():
+	os.system("python2 ~/.gimp-2.8/plug-ins/shutterstock_gui.py")
+	return
+
+register(
+	"helloWorldPlugin",
+
+	#blurb
+	"Saying Hello World",
+
+	#help
+	"Saying Hello to the World",
+
+	#author
+	"William Crandell <william@crandell.ws>",
+
+	#copyright
+	"William Crandell <william@crandell.ws>",
+
+	#date
+	"2015",
+
+	#menupath
+	"Hello World",
+
+	#imagetypes (use * for all, leave blank for none)
+	"",
+
+	#params
+	[],
+
+	#results
+	[],
+
+	#function (to call)
+	start,
+
+	#this can be included this way or the menu value can be directly prepended to the menupath
+	menu = "<Toolbox>/Hello/")
+
+main()
---
+++ shutterstock_gui.py
@@ -0,0 +1,23 @@
+from Tkinter import *
+
+class MyFirstGUI:
+	def __init__(self, master):
+		self.master = master
+		master.title("BIG BRAIN TIME")
+
+		self.label = Label(master, text="Test")
+		self.label.pack()
+
+		self.greet_button = Button(master, text="Do something", command=self.something)
+		self.greet_button.pack()
+
+		self.close_button = Button(master, text="Close", command=master.quit)
+		self.close_button.pack()
+
+	def something(self):
+		print('something')
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.geometry("400x400")
+root.mainloop()
