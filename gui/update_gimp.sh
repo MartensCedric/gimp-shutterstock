fb6c76b15eab261ffce007630f3afd769d4a4101
MartensCedric <cedricmartens98@gmail.com>
2020-01-25 14:32:43 -0500

    mv script

---
+++ gui/update_gimp.sh
@@ -0,0 +1,2 @@
+cp shutterstock_gui.py ~/.gimp-2.8/plug-ins/gui/shutterstock_gui.py
+cp shutterstock.py ~/.gimp-2.8/plug-ins/shutterstock.py
--- update_gimp.sh
+++
@@ -1,2 +0,0 @@
-cp shutterstock_gui.py ~/.gimp-2.8/plug-ins/shutterstock_gui.py
-cp shutterstock.py ~/.gimp-2.8/plug-ins/shutterstock.py
